e47ebfc312a3c4747eb2625ab741c6354d03c9b9
rahatsikz <rascal6271@gmail.com>
2022-11-23 19:23:34 +0600

    validation added

--- js/calculation.js
+++ js/calculation.js
@@ -1,12 +1,25 @@
 function getInputFieldValue(inputId) {
   const inputField = document.getElementById(inputId);
   const inputValue = parseFloat(inputField.value);
+  if (isNaN(inputValue)) {
+    alert("Please enter a number");
+    return;
+  }
+  if (inputField.value.includes("-")) {
+    alert("Please enter a positive number");
+    return;
+  }
   return inputValue;
 }
 
 function setTextElementValue(elementID, newValue) {
   const element = document.getElementById(elementID);
   element.innerText = newValue;
+}
+
+function clearInputField(inputId) {
+  const inputField = document.getElementById(inputId);
+  inputField.value = "";
 }
 
 let balance;
@@ -19,18 +32,35 @@ document.getElementById("btn-calculate").addEventListener("click", function () {
 
   const totalExpense = foodCost + rentCost + clothCost;
   balance = income - totalExpense;
-
-  setTextElementValue("expense-output", totalExpense);
-  setTextElementValue("balance-output", balance);
+  if (totalExpense > income) {
+    alert("You can't expense more than income");
+    return;
+  } else {
+    setTextElementValue("expense-output", totalExpense);
+    setTextElementValue("balance-output", balance);
+  }
+  clearInputField("food-input");
+  clearInputField("rent-input");
+  clearInputField("cloth-input");
 });
 
 document.getElementById("btn-save").addEventListener("click", function () {
   const income = getInputFieldValue("income-input");
   const savingPercent = getInputFieldValue("saving-input");
   const savings = income * (savingPercent / 100);
-  setTextElementValue("saving-output", savings);
 
   const remainingBalance = balance - savings;
   // console.log(remainingBalance);
-  setTextElementValue("remaining", remainingBalance);
+  if (savingPercent > 100) {
+    alert("Enter valid percentage");
+    return;
+  }
+  if (savings > balance) {
+    alert("You don't have enough balance");
+    return;
+  } else {
+    setTextElementValue("saving-output", savings);
+    setTextElementValue("remaining", remainingBalance);
+  }
+  clearInputField("income-input");
 });
